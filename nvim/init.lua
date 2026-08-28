93d161a21305135c4b716a325b98ff5808bbae70
leonardo.spilere <leonardo.spilere@deps.com.br>
2026-08-28 14:17:40 -0300

    Toggle tmux status bar on vim enter/leave
    - Disable tmux status bar when entering Neovim for more screen space
    - Re-enable tmux status bar when leaving Neovim
    - Only executes when running inside a tmux session (TMUX env var set)

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,20 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- disable tmux status bar on LazyVim startup
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.env.TMUX then
+      os.execute("tmux set status off")
+    end
+  end,
+})
+
+-- enable tmux status bar on LazyVim close
+vim.api.nvim_create_autocmd("VimLeave", {
+  callback = function()
+    if vim.env.TMUX then
+      os.execute("tmux set status on")
+    end
+  end,
+})
